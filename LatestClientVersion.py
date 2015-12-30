8a8c0f30a72cc48c83a5626320a9451f3afa320b
Luke Lu <lulu@linkedin.com>
2015-12-29 23:56:15 -0800

    lets see how box is...

--- LatestClientVersion.py
+++ LatestClientVersion.py
@@ -11,4 +11,4 @@ def get_latest_android_version_number():
 
 @latest_client_version.route('/latestVersionAndroid')
 def get_hymn():
-    return redirect("https://drive.google.com/file/d/0B3XbhMal4IMvVHJpN3E2X0duVmc/view?usp=sharing", code=302)
+    return redirect("https://app.box.com/s/f2rr3q41nw2z3tnqrm1pqlay7k1fynhg", code=302)
