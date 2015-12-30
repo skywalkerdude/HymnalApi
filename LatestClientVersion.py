34900a1aec8ad5bd56a91961ec28e40d2c6fc9e1
Luke Lu <lulu@linkedin.com>
2015-12-29 23:06:52 -0800

    can now get both latest version number and latest version (the apk itself). They will be updated in conjunction to where the api is hosted.

--- LatestClientVersion.py
+++ LatestClientVersion.py
@@ -1,10 +1,14 @@
 import simplejson as json
-from flask import Blueprint
+from flask import Blueprint, redirect
 
 latest_client_version = Blueprint('latest_client_verison', __name__)
 
 LATEST_CLIENT_VERSION = 'latest_client_version'
 
-@latest_client_version.route('/latestVersion')
-def get_hymn():
+@latest_client_version.route('/latestVersionNumberAndroid')
+def get_latest_android_version_number():
     return json.dumps({LATEST_CLIENT_VERSION : 2}, sort_keys=False)
+
+@latest_client_version.route('/latestVersionAndroid')
+def get_hymn():
+    return redirect("http://www.example.com", code=302)
