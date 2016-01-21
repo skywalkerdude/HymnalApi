9f099957acbac565e3ce15cbba6c57ca8ef56e14
Luke Lu <lulu@linkedin.com>
2016-01-21 01:51:38 -0800

    update latest client version

--- LatestClientVersion.py
+++ LatestClientVersion.py
@@ -7,8 +7,8 @@ LATEST_CLIENT_VERSION = 'latest_client_version'
 
 @latest_client_version.route('/latestVersionNumberAndroid')
 def get_latest_android_version_number():
-    return json.dumps({LATEST_CLIENT_VERSION : 2}, sort_keys=False)
+    return json.dumps({LATEST_CLIENT_VERSION : 3}, sort_keys=False)
 
 @latest_client_version.route('/latestVersionAndroid')
 def get_hymn():
-    return redirect("https://drive.google.com/file/d/0B3XbhMal4IMvWE9lOVdEazNkTVE/view?usp=sharing", code=302)
+    return redirect("https://drive.google.com/file/d/0B3XbhMal4IMvUEp4Q3J3eTcyd2s/view?usp=sharing", code=302)
