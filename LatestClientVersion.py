1effc3c826ea37f09fe1e62abaa00d180f690aa2
Luke Lu <skywalkerdude@gmail.com>
2017-08-05 22:21:35 -0700

    update client version from 10 to 12

--- LatestClientVersion.py
+++ LatestClientVersion.py
@@ -7,8 +7,8 @@ LATEST_CLIENT_VERSION = 'latest_client_version'
 
 @latest_client_version.route('/latestVersionNumberAndroid')
 def get_latest_android_version_number():
-    return json.dumps({LATEST_CLIENT_VERSION : 10}, sort_keys=False)
+    return json.dumps({LATEST_CLIENT_VERSION : 12}, sort_keys=False)
 
 @latest_client_version.route('/latestVersionAndroid')
 def get_hymn():
-    return redirect("https://drive.google.com/file/d/0B3XbhMal4IMvSFBQRi0zNnZDeHc/view?usp=sharing", code=302)
+    return redirect("https://drive.google.com/file/d/0B3XbhMal4IMvdnhCNlA3dmFueXc/view?usp=sharing", code=302)
