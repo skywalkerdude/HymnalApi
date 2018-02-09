6461a3d99e8318aa904ce45702c9e4eea23a40e3
Luke Lu <skywalkerdude@gmail.com>
2018-02-09 14:19:43 -0800

    Revert "update client version from 16 to 17"
    This reverts commit 304197a29a603d4babb8f36d6aef4a047d6a1125.

--- LatestClientVersion.py
+++ LatestClientVersion.py
@@ -7,8 +7,8 @@ LATEST_CLIENT_VERSION = 'latest_client_version'
 
 @latest_client_version.route('/latestVersionNumberAndroid')
 def get_latest_android_version_number():
-    return json.dumps({LATEST_CLIENT_VERSION : 17}, sort_keys=False)
+    return json.dumps({LATEST_CLIENT_VERSION : 16}, sort_keys=False)
 
 @latest_client_version.route('/latestVersionAndroid')
 def get_hymn():
-    return redirect("https://drive.google.com/file/d/17LfrEnTnyQaPjaJP_RGCs2Z1JVOJVH-m/view?usp=sharing", code=302)
+    return redirect("https://drive.google.com/file/d/0B3XbhMal4IMvSHkwTXBCVnlPT2s/view?usp=sharing", code=302)
