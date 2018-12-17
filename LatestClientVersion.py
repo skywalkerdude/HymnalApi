a6c5fa672763d241c8a551ebb0a2fd48eef2eeda
Luke Lu <skywalkerdude@gmail.com>
2018-12-16 23:39:35 -0800

    update client version from 26 to 27

--- LatestClientVersion.py
+++ LatestClientVersion.py
@@ -7,8 +7,8 @@ LATEST_CLIENT_VERSION = 'latest_client_version'
 
 @latest_client_version.route('/latestVersionNumberAndroid')
 def get_latest_android_version_number():
-    return json.dumps({LATEST_CLIENT_VERSION : 26}, sort_keys=False)
+    return json.dumps({LATEST_CLIENT_VERSION : 27}, sort_keys=False)
 
 @latest_client_version.route('/latestVersionAndroid')
 def get_hymn():
-    return redirect("https://drive.google.com/file/d/1UB3pLPMY21T8yDIwgqpjDa0afWTcnsDD/view?usp=sharing", code=302)
+    return redirect("https://drive.google.com/file/d/1dioiqFC_lBVbVz9JhqMdhOFu29HpM5cD/view?usp=sharing", code=302)
